ab389f9ed742466d666b864c23f3922ae324d88f
Jhony Vidal <jhonylopez147@hotmail.com>
2024-04-24 21:56:47 -0500

    last updates

--- src/modules/store/components/containerStore.component.tsx
+++ src/modules/store/components/containerStore.component.tsx
@@ -31,6 +31,7 @@ import { storeActions } from "../../../store/modules/store/store.slice";
 import { useNavigate } from "react-router";
 import { Product } from "../types";
 import { productExchange } from "../../exchangeProducts/types";
+import { CurrencyFormat } from "../../../utils/helpers";
 
 const cardStyle = {
   padding: '20px',
@@ -261,16 +262,16 @@ const ContainerStore = () => {
           {products.map((item: any) => {
             return (
               <Grid item xs={2.4} style={{ textAlign: "center" }} onClick={() => handleProduct(item)}>
-                <CardComponent
-                  style={cardStyle}
-                >
-                  {item.store.discount > 0 && (
+                 {item.store.discount > 0 && (
                     <div className="promotion">
                       <p>{item.store.discount}</p>
                       <p>%off</p>
                       <img src="icons/discount.png" alt=""/>
                     </div>
                   )}
+                <CardComponent
+                  style={cardStyle}
+                >
                   <img src={item.image} alt="" width={200} height={200}  style={{maxWidth: "100%"}}/>
                   <Typography style={storeStyles.card.title}>
                     {item.name}
@@ -282,7 +283,7 @@ const ContainerStore = () => {
                     {item.description.slice(0, 50)}
                   </Typography>
                   <Typography style={storeStyles.card.price}>
-                    $ {item.store.price}
+                    {CurrencyFormat(item.store.price)}
                   </Typography>
                 </CardComponent>
               </Grid>
@@ -324,11 +325,12 @@ const storeStyles = {
       ...weblysleekFontStyle,
       fontSize: "19px",
       fontWeight: "600",
-      height: "110px",
+      height: "70px",
+      marginTop: "10px"
     },
     subtitle: {
       ...weblysleekFontStyle,
-      fontSize: "13px",
+      fontSize: "16px",
     },
     content: {
       ...weblysleekFontStyle,
