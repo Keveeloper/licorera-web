14e2f17c3575a25e8c93de19372c07e9e875bf7a
Kevind <kevind171819@gmail.com>
2024-05-08 16:41:59 -0500

    Discount working in product search

--- src/modules/shared/header/components/nav/search/SearchBar.tsx
+++ src/modules/shared/header/components/nav/search/SearchBar.tsx
@@ -50,9 +50,9 @@ const SearchBar = () => {
 
     const handleBlur = () => {
         setTimeout(() => {
-            setSearchedText('');
-            setSearching(false);
-            dispatchClear(searchedActions.clearPersonalInfo());
+            // setSearchedText('');
+            // setSearching(false);
+            // dispatchClear(searchedActions.clearPersonalInfo());
         }, 500);
     }
 
