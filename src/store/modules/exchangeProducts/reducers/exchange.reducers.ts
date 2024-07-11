eacd46c94d9e76220c9a360c9e131fab551914bf
Keveloper <111926375+Bebeeloper@users.noreply.github.com>
2024-07-10 22:57:09 -0500

    Implementing MeExchange products wit API

--- src/store/modules/exchangeProducts/reducers/exchange.reducers.ts
+++ src/store/modules/exchangeProducts/reducers/exchange.reducers.ts
@@ -24,3 +24,26 @@ export const exchangeRejected = (
   state.error = action.payload || action.error;
 };
 
+
+export const exchangeMePending = (state: PromotionState) => {
+  state.loadingStatus = 'loading';
+  state.error = undefined;
+};
+
+export const exchangeMeFulfilled = (
+  state: PromotionState,
+  action: PayloadAction<any>
+) => {
+  state.state = action.payload.response;
+  state.loadingStatus = 'loaded';
+  // state.loadingStatus = 'loading';
+};
+
+export const exchangeMeRejected = (
+  state: PromotionState,
+  action: any
+) => {
+  state.loadingStatus = 'error';
+  state.error = action.payload || action.error;
+};
+
--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -50,3 +50,25 @@ export const  convertFormatDate = (cadenaFecha:string) => {
   
     return fechaFormateada;
   }
+
+export const  convertFormatDateNoHour = (cadenaFecha:string) => {
+
+    // Parsear la cadena de fecha
+    const fecha = new Date(cadenaFecha);
+
+    // Días de la semana y meses en español
+    const diasSemana = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
+    const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+    // Obtener información de la fecha
+    const diaSemana = diasSemana[fecha.getDay()];
+    const dia = fecha.getDate();
+    const mes = meses[fecha.getMonth()];
+    const año = fecha.getFullYear();
+    let horas = fecha.getHours();
+
+    // Formatear la fecha en el nuevo formato
+    const fechaFormateada = `${diaSemana}, ${dia} ${mes} ${año}`;
+
+    return fechaFormateada;
+}
